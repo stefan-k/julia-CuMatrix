4254773b2ba943dbd9e51b6c46c2f08cc216ed0f
Pavan Yalamanchili <contact@pavanky.com>
2012-04-23 03:15:01 -0400

    Converting jl_cuda_* to cuda_*
    - Also added finalizer functionsx

--- cuda.jl
+++ cuda.jl
@@ -1,82 +1,101 @@
-libCUMEM=dlopen("libcuplus")
-libCUBLAS=dlopen("libcublas")
+libcuplus=dlopen("libcuplus")
+libcublas=dlopen("libcublas")
+
+# Show cuda info
+function cuda_info()
+    ccall(dlsym(libcuplus, :cuda_info),
+          Void, ())
+end
 
 # Allocate device memory
-function jl_cuda_malloc(T::Type, count::Integer)
-    ptr::Ptr{Void} = ccall(dlsym(libCUMEM, :cuda_malloc),
+function cuda_malloc(T::Type, count::Integer)
+    ptr::Ptr{Void} = ccall(dlsym(libcuplus, :cuda_malloc),
                            Ptr{Void}, (Integer, ),
                            count * sizeof(T))
+    if ptr == C_NULL
+        # If unable to allocate, call garbage collector
+        gc()
+        # Try allocating again
+        ptr::Ptr{Void} = ccall(dlsym(libcuplus, :cuda_malloc),
+                               Ptr{Void}, (Integer, ),
+                               count * sizeof(T))
+    end
+
+    if ptr == C_NULL
+        # If still unable to allocate, error out
+        error("Can not allocate the required memory")
+    end
     return ptr
 end
-jl_cuda_malloc(T::Type, rows::Integer, cols::Integer) = jl_cuda_malloc(T, rows * cols)
-jl_cuda_malloc(T::Type, dims::(Integer, Integer)) = jl_cuda_malloc(T, dims[1] * dims[2])
-
+cuda_malloc(T::Type, rows::Integer, cols::Integer) = cuda_malloc(T, rows * cols)
+cuda_malloc(T::Type, dims::(Integer, Integer)) = cuda_malloc(T, dims[1] * dims[2])
 
 # Free device memory
-function jl_cuda_free(ptr::Ptr{Void})
-    ccall(dlsym(libCUMEM, :cuda_free),
+function cuda_free(ptr::Ptr{Void})
+    ccall(dlsym(libcuplus, :cuda_free),
           Void, (Integer, ),
           ptr)
 end
 
 # Copy from Host to Device Memory
-function jl_mem_device(dst::Ptr{Void}, src::Matrix)
+function mem_device(dst::Ptr{Void}, src::Matrix)
     bytes::Int32 = sizeof(eltype(src)) * numel(src)
-    ccall(dlsym(libCUMEM, :cuda_memcpy_h2d),
+    ccall(dlsym(libcuplus, :cuda_memcpy_h2d),
           Void, (Ptr{Void}, Ptr{Void}, Int32),
           dst, src, bytes)
           
 end
 
 # Copy from Device to Host Memory
-function jl_mem_host(dst::Matrix, src::Ptr{Void})
+function mem_host(dst::Matrix, src::Ptr{Void})
     bytes::Int32 = sizeof(eltype(dst)) * numel(dst)
-    ccall(dlsym(libCUMEM, :cuda_memcpy_d2h),
+    ccall(dlsym(libcuplus, :cuda_memcpy_d2h),
           Void, (Ptr{Void}, Ptr{Void}, Int32),
           dst, src, bytes)
           
 end
 
 # Copy from Device to Device Memory
-function jl_mem_copy(dst::Ptr{Void}, src::Ptr{Void}, bytes::Int32)
-    ccall(dlsym(libCUMEM, :cuda_memcpy_d2d),
+function mem_copy(dst::Ptr{Void}, src::Ptr{Void}, bytes::Int32)
+    ccall(dlsym(libcuplus, :cuda_memcpy_d2d),
           Void, (Ptr{Void}, Ptr{Void}, Int32),
           dst, src, bytes)
 end
 
 # Create uniform random Matrix
-function jl_curand(T::Type, ptr::Ptr{Void}, count::Integer)
+function cuda_rand(T::Type, ptr::Ptr{Void}, count::Integer)
     count = convert(Int32, count)
     IsFloat64::Bool = (T == Float64)
-    ccall(dlsym(libCUMEM, :cuda_rand),
+    ccall(dlsym(libcuplus, :cuda_rand),
           Void, (Ptr{Void}, Int32, Bool),
           ptr, count, IsFloat64)
 end
 
 # Create normal random Matrix
-function jl_curandn(T::Type, ptr::Ptr{Void}, count::Integer)
+function cuda_randn(T::Type, ptr::Ptr{Void}, count::Integer)
     count = convert(Int32, count)
     IsFloat64::Bool = (T == Float64)
-    ccall(dlsym(libCUMEM, :cuda_randn),
+    ccall(dlsym(libcuplus, :cuda_randn),
           Void, (Ptr{Void}, Int32, Bool),
           ptr, count, IsFloat64)
 end
 
-function jl_gemm{T}(transA::Char, transB::Char,
+# Matrix multiply
+function cuda_gemm{T}(transA::Char, transB::Char,
                     m::Int32, n::Int32, k::Int32,
                     alpha::T, A::Ptr{Void}, lda::Int32,
                     B::Ptr{Void}, ldb::Int32, beta::T,
                     C::Ptr{Void}, ldc::Int32)
     # FIXME: Fix the following fugliness
     if (T == Float32)
-        ccall(dlsym(libCUBLAS, :cublasSgemm),
+        ccall(dlsym(libcublas, :cublasSgemm),
               Void, (Char, Char, Int32, Int32, Int32,
                      Float32, Ptr{Float32}, Int32, Ptr{Float32}, Int32,
                      Float32, Ptr{Float32}, Int32),
               transA, transB, m, n, k, alpha, A, lda, B, ldb,
               beta, C, ldc)
     else
-        ccall(dlsym(libCUBLAS, :cublasDgemm),
+        ccall(dlsym(libcublas, :cublasDgemm),
               Void, (Char, Char, Int32, Int32, Int32,
                      Float64, Ptr{Float64}, Int32, Ptr{Float64}, Int32,
                      Float64, Ptr{Float64}, Int32),
@@ -85,30 +104,30 @@ function jl_gemm{T}(transA::Char, transB::Char,
     end
 end                  
 
-function jl_amax{T}(n::Int32, x::Ptr{Void}, incx::Int32, result::Ptr{Void})
-    ccall(dlsym(libCUBLAS, :cublasIdamax),
+function cuda_amax{T}(n::Int32, x::Ptr{Void}, incx::Int32, result::Ptr{Void})
+    ccall(dlsym(libcublas, :cublasIdamax),
           Void, (Int32, Ptr{Float64}, Int32, Ptr{Int32}),
           n, x, incx, result)
     return result
 end
 
-function jl_asum{T}(n::Int32, x::Ptr{Void}, incx::Int32, result::Ptr{Void})
-    ccall(dlsym(libCUBLAS, :cublasDasum),
+function cuda_asum{T}(n::Int32, x::Ptr{Void}, incx::Int32, result::Ptr{Void})
+    ccall(dlsym(libcublas, :cublasDasum),
           Void, (Int32, Ptr{Float64}, Int32, Ptr{Float64}),
           n, x, incx, result)
     return result
 end
 
-function jl_scal{T}(n::Int32, alpha::T, x::Ptr{Void}, incx::Int32)
+function cuda_scal{T}(n::Int32, alpha::T, x::Ptr{Void}, incx::Int32)
     if T == Float64
-        ccall(dlsym(libCUBLAS, :cublasDscal),
+        ccall(dlsym(libcublas, :cublasDscal),
               Void, (Int32, Float64, Ptr{Float64}, Int32),
               n, alpha, x, incx)
     end
 end
 
-function jl_dot{T}(n::Int32, x::Ptr{Void}, incx::Int32, y::Ptr{Void}, incy::Int32, res::Ptr{Void})
-    ccall(dlsym(libCUBLAS, :cublasDdot),
+function cuda_dot{T}(n::Int32, x::Ptr{Void}, incx::Int32, y::Ptr{Void}, incy::Int32, res::Ptr{Void})
+    ccall(dlsym(libcublas, :cublasDdot),
           Void, (Int32, Ptr{Void}, Int32, Ptr{Void}, Int32, Ptr{Void}),
           n, x, incx, y, incy, res)
 end
--- cumatrix.jl
+++ cumatrix.jl
@@ -11,22 +11,28 @@ type CuMatrix
         if T != Float32 && T != Float64
             error("No integer or boolean support yet")
         end
-        ptr = jl_cuda_malloc(T, dims)
-        new(T, ptr, dims)
+        ptr = cuda_malloc(T, dims)
+        Res = new(T, ptr, dims)
+        finalizer(Res, CuFree)
+        Res
     end
 
     # Copy Matrix from host
     function CuMatrix(in::Matrix)
         T = eltype(in)
         dims = size(in)
-        ptr = jl_cuda_malloc(T, dims)
-        jl_mem_device(ptr, in)
-        new(T, ptr, dims)
+        ptr = cuda_malloc(T, dims)
+        mem_device(ptr, in)
+        Res = new(T, ptr, dims)
+        finalizer(Res, CuFree)
+        Res
     end
 
     # Constructor with existing device pointer
     function CuMatrix(T::Type, ptr::Ptr{Void}, dims::(Integer, Integer))
-        new(T, ptr, dims)
+        Res = new(T, ptr, dims)
+        finalizer(Res, CuFree)
+        Res
     end
 
     # Aliasing
@@ -41,15 +47,15 @@ end
 # Get matrix from device to host
 function Array(in::CuMatrix)
     out = Array(in.T, in.dims[1], in.dims[2])
-    jl_mem_host(out, in.ptr)
+    mem_host(out, in.ptr)
     return out
 end
 
 # Perform a deep copy
 function copy(in::CuMatrix)
-    ptr = jl_cuda_malloc(in.T, in.dims)
+    ptr = cuda_malloc(in.T, in.dims)
     bytes::Int32 = in.dims[1] * in.dims[2] * sizeof(in.T)
-    jl_mem_copy(ptr, in.ptr, bytes)
+    mem_copy(ptr, in.ptr, bytes)
     CuMatrix(in.T, ptr, in.dims)
 end
     
@@ -57,4 +63,78 @@ end
 function print(in::CuMatrix)
     print("On GPU\n")
     print(Array(in), "\n")
+end
+
+# Freeing memory
+function CuFree(in::CuMatrix)
+    cuda_free(in.ptr)
+end
+
+# Random Number Generators
+function curand(T::Type, rows::Integer, cols::Integer)
+    out = CuMatrix(T, rows, cols)
+    cuda_rand(T, out.ptr, rows * cols)
+    return out
+end
+
+function curandn(T::Type, rows::Integer, cols::Integer)
+    # curand only supports randn for even number of elements
+    count = rows * cols
+    count += count & 1 # Increase the malloc size
+    ptr = cuda_malloc(T, count)
+    cuda_randn(T, ptr, count)
+    # But only use the required number of elements
+    CuMatrix(T, ptr, (rows, cols))
+end
+
+curand(rows::Integer, cols::Integer) = curand(Float32, rows, cols)
+curandn(rows::Integer, cols::Integer) = curandn(Float32, rows, cols)
+
+# BLAS Functions
+function (*)(A::CuMatrix, B::CuMatrix)
+    if (A.dims[2] != B.dims[1])
+        error("Inner dimension mismatch in Matrix multiply")
+    end
+    if (A.T != B.T)
+        error("Precision mismatch in Matrix multiply")
+    end
+    C = CuMatrix(A.T, (A.dims[1], B.dims[2]))
+
+    m = convert(Int32, C.dims[1])
+    n = convert(Int32, C.dims[2])
+    k = convert(Int32, B.dims[1])
+    
+    cuda_gemm('N', 'N', m, n, k,
+            one(A.T), A.ptr, m, B.ptr, k,
+            zero(A.T), C.ptr, m)
+    return C
+end
+
+function cuamax(A::CuMatrix)
+    result = CuMatrix(Int32, 1, 1)
+    # needs some more checking
+    n = convert(Int32, A.dims[1] > A.dims[2] ? A.dims[1] : A.dims[2])
+    cuda_amax(n,  A.ptr, int32(1), result.ptr)
+    return result
+end
+
+function cuasum(A::CuMatrix)
+    result = CuMatrix(A.T, 1, 1)
+    # needs some more checking
+    n = convert(Int32, A.dims[1] > A.dims[2] ? A.dims[1] : A.dims[2])
+    cuda_asum(n,  A.ptr, int32(1), result.ptr)
+    return result
+end
+
+function cuscal(A::CuMatrix, alpha)
+    # needs some more checking
+    n = convert(Int32, A.dims[1] > A.dims[2] ? A.dims[1] : A.dims[2])
+    cuda_scal(n, alpha, A.ptr, int32(1))
+end
+
+function cudot(A::CuMatrix, B::CuMatrix)
+    result = CuMatrix(A.T, 1, 1)
+    n = convert(Int32, A.dims[1] > A.dims[2] ? A.dims[1] : A.dims[2])
+    cuda_dot(n, A.ptr, int32(1), B.ptr, int32(1), result.ptr)
+    return result
 end
