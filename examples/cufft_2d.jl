d214ce71084931d084d54c1049d751eb1f3dbd32
Stefan Kroboth <stefan.kroboth@gmail.com>
2012-05-04 23:14:39 +0200

    added examples folder with example code for the CUFFT wrappers

---
+++ examples/cufft_2d.jl
@@ -0,0 +1,27 @@
+load("cumatrix.jl")
+
+print("CUFFT version: $(cufftGetVersion())\n\n")
+
+sx, sy = 16, 16
+#sx, sy = 512, 512
+#sx, sy = 1024, 1024
+
+# create CUFFT plan
+plan = cufftPlan2d(sx, sy, CUFFT_C2C)
+
+# create data
+h_data = ones(Complex64, sx, sy)
+d_data = CuMatrix(h_data)
+o_data = copy(d_data)
+d_data_inv = copy(d_data)
+
+# FFT
+print("FFT:\n")
+@time cufftExec(plan, d_data, o_data, CUFFT_FORWARD)
+print(o_data)
+println()
+
+# IFFT
+print("IFFT:\n")
+@time cufftExec(plan, o_data, d_data_inv, CUFFT_INVERSE)
+print(d_data_inv*float32(1.0/(sx*sy)))
